1836a693a2458eb9afa2d53341648034209a72b4
Akhil <akhil.gunthati@gmail.com>
2025-10-03 16:03:48 +0530

    updating

--- Pointers/CharCount.cpp
+++ Pointers/CharCount.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main(){
     string str = "Akhil";
-    char *ptr;
+    char *ptr=&str[0];
     
+    int count = 0;
+    while (*ptr != '\0') {
+        //*ptr = &str[0];
+        count++;
+        ptr++;
+    }
+    cout << "Charcter count in the str:" << count;
 }
